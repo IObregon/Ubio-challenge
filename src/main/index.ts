e1fd12152cb7599addf58c617978b9c85e7acaca
IObregon <ikerobregonreigosa@gmail.com>
2019-12-08 00:52:59 +0100

    Created DELETE /:group/:id endpoint.

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,6 +14,11 @@ app.post('/:group/:id', (req: express.Request,res: express.Response) => {
   res.json(instanceApi.create({group: req.params['group'], id: req.params['id'], meta: req.body}));
 });
 
+app.delete('/:group/:id', (req: express.Request,res: express.Response) => {
+  res.status(204);
+  res.json(instanceApi.delete({group: req.params['group'], id: req.params['id']}));
+});
+
 
 app.listen(5000, () => {
   console.log('server started on port 5000');
