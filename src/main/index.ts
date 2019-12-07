8ded44e7ad66e45d86f9b3aa52f6da535d6e1071
IObregon <ikerobregonreigosa@gmail.com>
2019-12-07 23:35:59 +0100

    Created POST /:group/:id endpoint.

---
+++ src/main/index.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser'
+
+import InstanceApi from './api/InstancesApi'
+import Group from './types/Group';
+
+const app = express();
+const groups:Group[] = [];
+const instanceApi = new InstanceApi(groups);
+
+app.use(bodyParser.json());
+
+app.post('/:group/:id', (req: express.Request,res: express.Response) => {
+  res.json(instanceApi.create({group: req.params['group'], id: req.params['id'], meta: req.body}));
+});
+
+
+app.listen(5000, () => {
+  console.log('server started on port 5000');
+})
