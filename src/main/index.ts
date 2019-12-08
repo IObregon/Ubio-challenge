7ba86b151cac8dbb5e22a3db60f83b0884987aa4
IObregon <ikerobregonreigosa@gmail.com>
2019-12-08 01:02:07 +0100

    Created GET / endpoint.

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,21 +3,27 @@ import * as bodyParser from 'body-parser'
 
 import InstanceApi from './api/InstancesApi'
 import Group from './types/Group';
+import GroupsApi from './api/GroupsApi';
 
 const app = express();
-const groups:Group[] = [];
+const groups: Group[] = [];
 const instanceApi = new InstanceApi(groups);
+const groupApi = new GroupsApi(groups);
 
 app.use(bodyParser.json());
 
-app.post('/:group/:id', (req: express.Request,res: express.Response) => {
-  res.json(instanceApi.create({group: req.params['group'], id: req.params['id'], meta: req.body}));
+app.post('/:group/:id', (req: express.Request, res: express.Response) => {
+  res.json(instanceApi.create({ group: req.params['group'], id: req.params['id'], meta: req.body }));
 });
 
-app.delete('/:group/:id', (req: express.Request,res: express.Response) => {
+app.delete('/:group/:id', (req: express.Request, res: express.Response) => {
   res.status(204);
-  res.json(instanceApi.delete({group: req.params['group'], id: req.params['id']}));
+  res.json(instanceApi.delete({ group: req.params['group'], id: req.params['id'] }));
 });
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json(groupApi.findAll());
+})
 
 
 app.listen(5000, () => {
